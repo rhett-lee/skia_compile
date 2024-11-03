cc4c21997340710a98f74253c1bdd8de20631b55
rhett-lee <rhettli@hotmail.com>
2024-11-03 21:27:23 +0800

    增加OpenSuse和通信UOS系统下编译Skia的参考文档。

---
+++ source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# gcc/g++ 14.2
+export PATH=/home/develop/install/gcc-14.2.0/bin/:$PATH
+export LD_LIBRARY_PATH=/home/develop/install/gcc-14.2.0/lib64/:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=/home/develop/install/gcc-14.2.0/include/c++/14.2.0/:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/home/develop/install/gcc-14.2.0/include/c++/14.2.0/:$CPLUS_INCLUDE_PATH
+
