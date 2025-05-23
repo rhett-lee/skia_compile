5124c3f10059584582d40fc0d5d59fdcaca5932b
rhett-lee <rhettli@hotmail.com>
2025-05-23 21:08:39 +0800

    完善Windows脚本：增加一键完成所有操作的脚本。

---
+++ Winddows/build_skia_all_in_one.bat
@@ -0,0 +1,104 @@
+echo OFF
+
+echo Checking the necessary software
+where git.exe >nul 2>&1
+if %errorlevel% equ 0 (
+    echo git.exe found at:  
+    where git.exe
+) else (
+    echo git.exe not found in PATH
+    exit /b 1
+)
+
+where python3.exe >nul 2>&1
+if %errorlevel% equ 0 (
+    echo python3.exe found at:  
+    where python3.exe
+) else (
+    echo python3.exe not found in PATH
+    exit /b 1
+)
+
+where python.exe >nul 2>&1
+if %errorlevel% equ 0 (
+    echo python.exe found at:  
+    where python.exe
+) else (
+    echo python.exe not found in PATH
+    exit /b 1
+)
+
+if exist "C:\LLVM\bin\clang.exe" (
+    echo clang.exe found at: C:\LLVM\bin\clang.exe
+) else (
+    echo clang.exe not found in default location
+    exit /b 1
+)
+
+if exist "C:\LLVM\bin\clang++.exe" (
+    echo clang++.exe found at: C:\LLVM\bin\clang++.exe
+) else (
+    echo clang++.exe not found in default location
+    exit /b 1
+)
+
+set retry_delay=10     ;# Wait 10 seconds instead of 5
+:retry_clone_skia
+git clone https://github.com/google/skia.git
+if %errorlevel% neq 0 (
+    timeout /t %retry_delay% >nul
+    goto retry_clone_skia
+)
+
+if not exist ".\skia" (
+    echo clone skia failed!
+    exit /b 1
+)
+
+:retry_clone_skia_compile
+git clone https://github.com/rhett-lee/skia_compile
+if %errorlevel% neq 0 (
+    timeout /t %retry_delay% >nul
+    goto retry_clone_skia_compile
+)
+
+if not exist ".\skia_compile" (
+    echo clone retry_clone_skia_compile failed!
+    exit /b 1
+)
+
+if not exist ".\skia_compile\skia.2025-05-17.src.zip" (
+    echo ".\skia_compile\skia.2025-05-17.src.zip" not found!
+    exit /b 1
+)
+
+cd skia
+git checkout 22d94029d2ac36dfac8d631fa0d1b467f0ab45a7
+if %errorlevel% neq 0 (
+    echo git checkout skia failed!
+    exit /b 1
+)
+cd ..
+
+powershell -command "Expand-Archive -Path 'skia_compile\skia.2025-05-17.src.zip' -DestinationPath .\skia\ -Force"
+if %errorlevel% neq 0 (
+    echo ".\skia_compile\skia.2025-05-17.src.zip" Expand-Archive failed!
+    exit /b 1
+)
+
+cd skia
+.\bin\gn.exe gen out/llvm.x64.debug --ide="vs2022" --sln="skia" --args="target_cpu=\"x64\" cc=\"clang\" cxx=\"clang++\" clang_win=\"C:/LLVM\" is_trivial_abi=false is_official_build=true skia_use_libwebp_encode=false skia_use_libwebp_decode=false skia_use_libpng_encode=false skia_use_libpng_decode=false skia_use_zlib=false skia_use_libjpeg_turbo_encode=false skia_use_libjpeg_turbo_decode=false skia_enable_fontmgr_win_gdi=false skia_use_icu=false skia_use_expat=false skia_use_xps=false skia_enable_pdf=false skia_use_wuffs=false skia_enable_svg=true skia_use_expat=true skia_use_system_expat=false is_debug=false extra_cflags=[\"-DSK_DISABLE_LEGACY_PNG_WRITEBUFFER\",\"/MTd\"]"
+.\bin\ninja.exe -C out/llvm.x64.debug
+
+.\bin\gn.exe gen out/llvm.x64.release --ide="vs2022" --sln="skia" --args="target_cpu=\"x64\" cc=\"clang\" cxx=\"clang++\" clang_win=\"C:/LLVM\" is_trivial_abi=false is_official_build=true skia_use_libwebp_encode=false skia_use_libwebp_decode=false skia_use_libpng_encode=false skia_use_libpng_decode=false skia_use_zlib=false skia_use_libjpeg_turbo_encode=false skia_use_libjpeg_turbo_decode=false skia_enable_fontmgr_win_gdi=false skia_use_icu=false skia_use_expat=false skia_use_xps=false skia_enable_pdf=false skia_use_wuffs=false skia_enable_svg=true skia_use_expat=true skia_use_system_expat=false is_debug=false extra_cflags=[\"-DSK_DISABLE_LEGACY_PNG_WRITEBUFFER\",\"/MT\"]"
+.\bin\ninja.exe -C out/llvm.x64.release
+
+.\bin\gn.exe gen out/llvm.x86.release --ide="vs2022" --sln="skia" --args="target_cpu=\"x86\" cc=\"clang\" cxx=\"clang++\" clang_win=\"C:/LLVM\" is_trivial_abi=false is_official_build=true skia_use_libwebp_encode=false skia_use_libwebp_decode=false skia_use_libpng_encode=false skia_use_libpng_decode=false skia_use_zlib=false skia_use_libjpeg_turbo_encode=false skia_use_libjpeg_turbo_decode=false skia_enable_fontmgr_win_gdi=false skia_use_icu=false skia_use_expat=false skia_use_xps=false skia_enable_pdf=false skia_use_wuffs=false skia_enable_svg=true skia_use_expat=true skia_use_system_expat=false is_debug=false extra_cflags=[\"-DSK_DISABLE_LEGACY_PNG_WRITEBUFFER\",\"/MT\"]"
+.\bin\ninja.exe -C out/llvm.x86.release
+
+.\bin\gn.exe gen out/llvm.x86.debug --ide="vs2022" --sln="skia" --args="target_cpu=\"x86\" cc=\"clang\" cxx=\"clang++\" clang_win=\"C:/LLVM\" is_trivial_abi=false is_official_build=true skia_use_libwebp_encode=false skia_use_libwebp_decode=false skia_use_libpng_encode=false skia_use_libpng_decode=false skia_use_zlib=false skia_use_libjpeg_turbo_encode=false skia_use_libjpeg_turbo_decode=false skia_enable_fontmgr_win_gdi=false skia_use_icu=false skia_use_expat=false skia_use_xps=false skia_enable_pdf=false skia_use_wuffs=false skia_enable_svg=true skia_use_expat=true skia_use_system_expat=false is_debug=false extra_cflags=[\"-DSK_DISABLE_LEGACY_PNG_WRITEBUFFER\",\"/MTd\"]"
+.\bin\ninja.exe -C out/llvm.x86.debug
+cd ..
+
+echo.
+echo finished.
